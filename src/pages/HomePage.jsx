45670c1c01908fd20aaeed30cc3370b72144514e
Julian Lopez <jlopezsa@gmail.com>
2022-03-10 13:36:43 -0500

    Add folder pages with jsx and scss files

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,17 @@
+import './HomePage.scss';
+import React from 'react'
+
+const HomePage = (props) =>{
+
+  return(
+    <div className="">
+      <h1 className="">Hello ParkingApp from React</h1>
+      {/* Header */}
+      {/* Searching */}
+      {/* Body */}
+      {/* Footer */}
+    </div>
+  )
+}
+
+export default HomePage
---
+++ src/pages/Login.jsx
@@ -0,0 +1,15 @@
+import './Login.scss';
+import React from 'react'
+
+const Login = (props) =>{
+
+  return(
+    <div className="">
+      {/* Header */}
+      {/* Register */}
+      {/* Footer */}
+    </div>
+  )
+}
+
+export default Login
