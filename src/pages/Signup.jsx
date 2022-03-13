55386248b3b50efbf8a44c1b5cbe7c21f42f8311
lmportoesq <lmportoesq@gmail.com>
2022-03-13 18:40:06 -0500

    making basic changes

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -11,6 +11,7 @@ function Signup() {
           <input className="main-container__form--input" type="password" name="confirm-password" placeholder="Confirmar contraseña" />
 
           <input className="main-container__form--submit" type="submit" name="submit" value="Registrar" />
+          <Link to="/login">Ya tienes una cuenta? Ingresa ahora</Link>
         </form>
       </div>
     </>
