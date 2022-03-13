dfef0813f2718299592b47c779e805bd92e8015a
Julian Lopez <jlopezsa@gmail.com>
2022-03-13 00:45:48 -0500

    Views component structures as atomic concept

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,17 +1,7 @@
 import './HomePage.scss';
 import React from 'react'
 import Footer from '../components/Footer/Footer';
-import ViewsFigLeft from '../components/ViewsFigLeft/ViewsFigLeft';
-import ViewsFigRight from '../components/ViewsFigRight/ViewsFigRight';
-import ViewsFigTopLeft from '../components/ViewsFigTopLeft/ViewsFigTopLeft';
-import ViewsFigTopRight from '../components/ViewsFigTopRight/ViewsFigTopRight';
-import leftFig from '../figures/views-Parking-bro.svg';
-import rigthFig from '../figures/views-navigator-cuate.svg';
-import parkingWoman from '../figures/views-Parking-woman.svg';
-import ViewsHorizontal from '../components/ViewsHorizontal/ViewsHorizontal';
-
-
-
+import Views from '../components/Views/Views';
 
 const cities = [
   { "hrefLink": "https://es.reactjs.org/", "ciudad": "Bogotá" },
@@ -26,14 +16,7 @@ const HomePage = (props) =>{
   return(
     <div className="container-home">
       <h1 className="">Hello ParkingApp from React</h1>
-      <ViewsFigLeft title={'El problema'} figure={leftFig} text={'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit cupiditate nesciunt hic nam. Magni, illo. Dignissimos at adipisci consequatur? Esse vitae cumque eius vero nihil atque eveniet, nostrum autem voluptatum? Adipisci iste, vel rerum quidem vitae natus voluptas laboriosam atque repudiandae.'}/>
-
-      <ViewsFigRight title={'¿Qué hacemos?'} figure={rigthFig} text={'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit cupiditate nesciunt hic nam. Magni, illo. Dignissimos at adipisci consequatur? Esse vitae cumque eius vero nihil atque eveniet, nostrum autem voluptatum? Adipisci iste, vel rerum quidem vitae natus voluptas laboriosam atque repudiandae.'}/>
-
-      <ViewsFigTopLeft title={'¿Cómo lo hacemos?'} figure={parkingWoman} text={'Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime assumenda soluta, quaerat mollitia sequi explicabo eaque dolor voluptatum animi dolorum magni beatae laboriosam? Quia, excepturi. Veniam doloremque aperiam deserunt nisi.'}/>
-
-      <ViewsHorizontal />
-
+      <Views />
       <Footer cities={cities} />
     </div>
   )
