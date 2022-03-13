3fa1bc4ada0dec1511aa94f64b81686199062398
lmportoesq <lmportoesq@gmail.com>
2022-03-12 22:08:38 -0500

    add react-router-dom dependencie

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,6 +1,24 @@
+import './Signup.scss';
 function Signup(){
   return(
-    <h1>Soy el modulo sign-up</h1>
+    <>
+      <div className="main-container">
+      <form className="main-container__form">
+        <h2>Registrarse</h2>
+        <input className="main-container__form--input" type="text" name="name" placeholder="Nombre"/>
+        <input className="main-container__form--input" type="text" name="last-name" placeholder="Apellidos"/>
+        <input className="main-container__form--input" type="email" name="email" placeholder="emal"/>
+        <input className="main-container__form--input" type="password" name="password" placeholder="Contraseña"/>
+        <input className="main-container__form--input" type="password" name="confirm-password" placeholder="Confirmar contraseña"/>
+
+        <input className="main-container__form--submit" type="submit" name="submit" value="Registrar"/>
+        <a href="#">Ya tienes una cuenta? Ingresa ahora</a>
+
+      </form>
+    </div>
+    </>
   )
 }
+
 export default Signup;
+
