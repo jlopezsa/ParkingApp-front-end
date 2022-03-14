c1800f1c29c274b4eb2ae6291e75f2473ffadd94
lmportoesq <lmportoesq@gmail.com>
2022-03-13 20:04:00 -0500

    add styles scss

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -4,10 +4,10 @@ function Signup() {
   return (
     <>
         <form className="form">
-          <h2>Registrarse</h2>
+          <h2 className='form__h2'>Registrarse</h2>
           <input className="form__input" type="text" name="name" placeholder="Nombre" />
           <input className="form__input" type="text" name="last-name" placeholder="Apellidos" />
-          <input className="mform__input" type="email" name="email" placeholder="emal" />
+          <input className="form__input" type="email" name="email" placeholder="email" />
           <input className="form__input" type="password" name="password" placeholder="Contraseña" />
           <input className="form__input" type="password" name="confirm-password" placeholder="Confirmar contraseña" />
 
