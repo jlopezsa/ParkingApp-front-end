f85042d1aa5ae0247f3a7ae27d57d5db0da78def
Julian Lopez <jlopezsa@gmail.com>
2022-03-14 00:45:27 -0500

    feature/Simplify the HomePage.jsx component

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,21 +4,13 @@ import Footer from '../components/Footer/Footer';
 import Views from '../components/Views/Views';
 import ParkingList from '../components/ParkingList/ParkingList';
 
-const cities = [
-  { "hrefLink": "https://es.reactjs.org/", "ciudad": "Bogotá" },
-  { "hrefLink": "https://es.reactjs.org/", "ciudad": "Cali" },
-  { "hrefLink": "https://es.reactjs.org/", "ciudad": "Medellín" },
-  { "hrefLink": "https://es.reactjs.org/", "ciudad": "Bucaramanga" },
-  { "hrefLink": "https://es.reactjs.org/", "ciudad": "Santa Marta" },
-  { "hrefLink": "https://es.reactjs.org/", "ciudad": "Cartagena" },
-  { "hrefLink": "https://es.reactjs.org/", "ciudad": "Barranquilla" }]
 
 const HomePage = (props) =>{
   return(
     <div className="container-home">
       <h1 className="">Hello ParkingApp from React</h1>
       <Views />
-      <Footer cities={cities} />
+      <Footer />
       <ParkingList />
     </div>
   )
