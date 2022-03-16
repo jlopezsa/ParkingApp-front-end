dff80725bfdf867db52378b9f9d1565f2ce629a6
lmportoesq <lmportoesq@gmail.com>
2022-03-15 20:23:13 -0500

    call Cities component and coment the others

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -6,17 +6,19 @@ import Views from '../components/Views/Views';
 import ParkingList from '../components/ParkingList/ParkingList';
 import Signup from './Signup';
 import Login from './Login';
+import Cities from './Cities';
 
 
 const HomePage = (props) =>{
   return(
     <div className="container-home">
       <h1 className="">Hello ParkingApp from React</h1>
-      <Views />
+      {/*<Views />
       <Footer />
       <ParkingList />
       <Signup />
-      <Login />
+      <Login />*/}
+      <Cities />
     </div>
   )
 }
