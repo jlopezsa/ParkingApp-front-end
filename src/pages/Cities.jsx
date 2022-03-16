74f1fc4d2b90c343ce74488f91947cb39aa1213d
lmportoesq <lmportoesq@gmail.com>
2022-03-15 20:39:27 -0500

    create City.jsx file to search by cities from Cities component

--- src/pages/Cities.jsx
+++ src/pages/Cities.jsx
@@ -1,15 +1,24 @@
-import {NavLink} from 'react-router-dom';
+import {BrowserRouter,Routes,Route,NavLink} from 'react-router-dom';
+import City from './City';
+
 function Cities() {
   return (
-    <div className="container">
-      <ul>
-        <li id="Bogota"><NavLink to ="/Bogota" href="#">Bogotá</NavLink></li>
-        <li><NavLink id="Cali" to="/Cali">Cali</NavLink></li>
-        <li><NavLink id="Medellin" to="#">Medellín</NavLink></li>
-        <li><NavLink id="Cartagena" to="#">Cartagena</NavLink></li>
-        <li><NavLink id="Bucaramanga" to="#">Bucaramanga</NavLink></li>
-      </ul>
-    </div>
+    <>
+      <BrowserRouter>
+        <nav className="container">
+          <ul>
+            <li id="Bogota"><NavLink to ="/City" href="#">Bogotá</NavLink></li>
+            <li><NavLink id="Cali" to="/City">Cali</NavLink></li>
+            <li><NavLink id="Medellin" to="/City">Medellín</NavLink></li>
+            <li><NavLink id="Cartagena" to="/City">Cartagena</NavLink></li>
+            <li><NavLink id="Bucaramanga" to="/City">Bucaramanga</NavLink></li>
+        </ul>
+        </nav>
+        <Routes>
+          <Route path="/City" element={<City />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   )
 }
 export default Cities;
