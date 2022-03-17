3e3f44b26379621c5a9bcfe80180d3358a4ebdb2
lmportoesq <lmportoesq@gmail.com>
2022-03-16 21:47:34 -0500

    feact: call to SearchDateHour component and coment the others

--- src/components/Searchdayhour/Searchdayhour.jsx
+++ src/components/Searchdayhour/Searchdayhour.jsx
@@ -1,6 +1,14 @@
 function SearchDayHour(){
   return(
-    <h1>Soy el componente SearchDayHour</h1>
+    <Div className="container">
+      <label>Entrada</label>
+      <label>Salida</label>
+      <input type="date" name="stardate" placeholder="ingrese fecha" />
+      <input type="date" name="enddate" placeholder="ingrese fecha" />
+
+      <input type="time" name="startime" placeholder="ingrese hora" />
+      <input type="time" name="endtime" placeholder="ingrese hora" />
+    </Div>
   )
 }
 export default SearchDayHour;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -6,17 +6,19 @@ import Views from '../components/Views/Views';
 import ParkingList from '../components/ParkingList/ParkingList';
 import Signup from './Signup';
 import Login from './Login';
+import SearchDayHour from '../components/Searchdayhour/Searchdayhour';
 
 
 const HomePage = (props) =>{
   return(
     <div className="container-home">
       <h1 className="">Hello ParkingApp from React</h1>
-      <Views />
+      {/*<Views />
       <Footer />
       <ParkingList />
       <Signup />
-      <Login />
+      <Login />*/}
+      <SearchDayHour />
     </div>
   )
 }
