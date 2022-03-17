f9a59309367a08964f8cfdb51b2bd490d0a62727
lmportoesq <82108153+lmportoesq@users.noreply.github.com>
2022-03-17 16:47:56 -0500

    Feacture cities porto (#15)
    * create file cities.html
    
    * create Cities.jsx component
    
    * call Cities component and coment the others
    
    * create City.jsx file to search by cities from Cities component
    
    * move every component to the correct folder Login, Singup and Cities
    
    * create file Cities.scss
    
    * feact: imported file App.scss
    
    Co-authored-by: lmportoesq <lmportoesq@gmail.com>

---
+++ src/pages/City.jsx
@@ -0,0 +1,6 @@
+function City(){
+  return(
+    <h1>Soy la página city</h1>
+  )
+}
+export default City;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,19 +4,21 @@ import React from 'react'
 import Footer from '../components/Footer/Footer';
 import Views from '../components/Views/Views';
 import ParkingList from '../components/ParkingList/ParkingList';
-import Signup from './Signup';
-import Login from './Login';
+import Signup from '../components/Signup/Signup';
+import Login from '../components/Login/Login';
+import Cities from '../components/Cities/Cities';
 
 
 const HomePage = (props) =>{
   return(
     <div className="container-home">
       <h1 className="">Hello ParkingApp from React</h1>
-      <Views />
+      {/*<Views />
       <Footer />
       <ParkingList />
       <Signup />
-      <Login />
+      <Login />*/}
+      <Cities />
     </div>
   )
 }
