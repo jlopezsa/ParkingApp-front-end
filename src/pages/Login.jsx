973b6d967eaf61ca9e1d3a3b649510fc61ab5253
lmportoesq <lmportoesq@gmail.com>
2022-03-21 18:50:23 -0500

    create Login page

---
+++ src/pages/Login.jsx
@@ -0,0 +1,17 @@
+/* eslint-disable */
+import React from 'react'
+
+import Footer from '../components/Footer/Footer';
+import Header from '../components/Header/Header';
+
+function Login(){
+  return(
+    <div className="container-login">
+      <Header/>
+      <Login />
+      <Footer />
+    </div>
+  )
+}
+
+export default Login;
