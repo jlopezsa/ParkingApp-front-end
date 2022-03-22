d2165f14ac2e0095f912268b5f32838e627d0d48
lmportoesq <82108153+lmportoesq@users.noreply.github.com>
2022-03-21 20:41:02 -0500

    Feact/signup page (#21)
    * create page sign-up
    
    * delete border at Signup.scss file
    
    Co-authored-by: lmportoesq <lmportoesq@gmail.com>

--- src/pages/sign-up.jsx
+++ src/pages/sign-up.jsx
@@ -1,7 +1,18 @@
-function SignUp() {
-  return (
-    <h1>Soy la página SignUp</h1>
-  );
+/* eslint-disable */
+import React from 'react'
+
+import Footer from '../components/Footer/Footer';
+import Header from '../components/Header/Header';
+import SignUp from '../components/Signup/Signup';
+
+const SingUp = (props) =>{
+  return(
+    <div className="container-signup">
+      <Header/>
+      <SignUp/>
+      <Footer />
+    </div>
+  )
 }
 
-export default SignUp;
+export default SingUp;
