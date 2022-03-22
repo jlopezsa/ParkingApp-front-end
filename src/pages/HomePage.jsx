879ff18c8c6dc74f4eefb8cf8f6f164107eaba76
Julian Lopez <jlopezsa@gmail.com>
2022-03-21 20:37:39 -0500

    feat: Finish HomePage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,22 +1,24 @@
-/* eslint-disable */
-import React from 'react'
+import React from 'react';
+import './HomePage.scss';
 
 import Footer from '../components/Footer/Footer';
 import Views from '../components/Views/Views';
 import Header from '../components/Header/Header';
 import SearchBar from '../components/SearchBar/SearchBar';
-import Cities from '../components/Cities/Cities'
+import Cities from '../components/Cities/Cities';
 
-const HomePage = (props) =>{
-  return(
+function HomePage() {
+  return (
     <div className="container-home">
-      <Header/>
-      <Cities />
-      <SearchBar />
+      <Header />
+      <div className="container-home__citysearch">
+        <Cities />
+        <SearchBar />
+      </div>
       <Views />
       <Footer />
     </div>
-  )
+  );
 }
 
-export default HomePage
+export default HomePage;
