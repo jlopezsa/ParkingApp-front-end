4b2de6de246373521eaaceaf67b16d2ac2cf97e9
lmportoesq <lmportoesq@gmail.com>
2022-03-25 06:58:43 -0500

    create component CreateParking

--- src/components/CreateParking/CreateParking.jsx
+++ src/components/CreateParking/CreateParking.jsx
@@ -2,7 +2,20 @@ import React from 'react';
 
 function CreateParking() {
   return (
-    <h2>Ingrese datos del parqueadero</h2>
+    <div className="container">
+      <h2>Ingrese datos del parqueadero</h2>
+      <form className="container__form">
+        <input className="container__form--input" type="text" name="name" placeholder="Nombre del parqueadero" />
+        <input className="container__form--input" type="text" name="city" placeholder="Ciudad" />
+        <input className="container__form--input" type="number" name="price" placeholder="Tarifa" />
+        <input className="container__form--input" type="time" name="startTime" placeholder="Hora de apertura" />
+        <input className="container__form--input" type="time" name="endTime" placeholder="Hora de cierre" />
+        <input className="container__form--input" type="number" name="quotas" placeholder="Total cupos disponibles" />
+        <input className="container__form--input" type="imagen" name="image" placeholder="Imagen" />
+        <button type="submit" className="container__form--Button">Registrar</button>
+        <button type="submit" className="container__form--Button">Cancelar</button>
+      </form>
+    </div>
   );
 }
 export default CreateParking;
