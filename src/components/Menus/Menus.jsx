f7de8d252f5b14164680a58eda9ab9312979626c
Julian Lopez <jlopezsa@gmail.com>
2022-03-29 00:42:31 -0500

    feat: Menu hamburegr without styles

--- src/components/Menus/Menus.jsx
+++ src/components/Menus/Menus.jsx
@@ -1,4 +1,6 @@
+/* eslint-disable */
 import { BrowserRouter, Routes, Route, NavLink } from 'react-router-dom';
+// import { slide as Menu } from 'react-burger-menu'
 import HomePage from '../../pages/HomePage';
 import SignupPage from '../../pages/sign-up';
 import City from '../../pages/City';
@@ -18,6 +20,22 @@ function Menus() {
           <li><NavLink className="menu__list--inactive" to="/Search">Busqueda</NavLink></li>
         </ul>
       </nav>
+
+      <input type="checkbox" id="hamburger-input" className="burger-shower" />
+      <label id="hamburger-menu" htmlFor="hamburger-input">
+        <nav id="sidebar-menu">
+          <h3>Menu</h3>
+          <ul className="hamburguer-menu__list">
+            <li><NavLink className="hamburguer__list--inactive" to="/">Mi cuenta</NavLink></li>
+            <hr />
+            <li><NavLink className="hamburguer__list--inactive" to="/LogIn">Ingresar</NavLink></li>
+            <li><NavLink className="hamburguer__list--inactive" to="/LogUp">Registrar</NavLink></li>
+            <li><NavLink className="hamburguer__list--inactive" to="/Cities">Ciudades</NavLink></li>
+            <li><NavLink className="hamburguer__list--inactive" to="/Search">Busqueda</NavLink></li>
+          </ul>
+        </nav>
+      </label>
+
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/LogUp" element={<SignupPage />} />
