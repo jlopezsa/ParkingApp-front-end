07c15cfcf34c30b7c66fda5a1b1582699d248c49
Julian Lopez <jlopezsa@gmail.com>
2022-03-29 19:02:23 -0500

    feat: Add header and footer in the Booking page

--- src/pages/Booking.jsx
+++ src/pages/Booking.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 
+import Footer from '../components/Footer/Footer';
+import Header from '../components/Header/Header';
+
 function Booking() {
   return (
     <div>
+      <Header />
       <h1>Soy la página Booking</h1>
+      <Footer />
     </div>
   );
 }
