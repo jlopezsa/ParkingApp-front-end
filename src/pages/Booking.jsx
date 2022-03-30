de8a4e62a50cb1116bbccdb8aa243ae8f5f687ed
Julian Lopez <jlopezsa@gmail.com>
2022-03-29 21:14:15 -0500

    feat: Add account information, aditional informations

--- src/pages/Booking.jsx
+++ src/pages/Booking.jsx
@@ -1,13 +1,35 @@
 import React from 'react';
+import BookingSummary from '../components/BookingSummary/BookingSummary';
+import './Booking.scss';
 
 import Footer from '../components/Footer/Footer';
 import Header from '../components/Header/Header';
 
 function Booking() {
   return (
-    <div>
+    <div className="container-booking">
       <Header />
-      <h1>Soy la página Booking</h1>
+      <div className="booking-body">
+        <div className="booking-body__account">
+          <h5>Información de cuenta</h5>
+          <p>Registrado como: jhondoe@gmail.com</p>
+          <p id="p_exit">Salir</p>
+        </div>
+        <div className="booking-body__aditional">
+          <h5>Informaciones adicionales</h5>
+          <div className="booking-body__aditional--inputs">
+            <input id="contact_phone" type="tel" name="contactPhone" value="Telefono" />
+            <input id="placa_carro" type="input" name="placaCarro" value="Placa del vehículo" />
+            <textarea id="comments" type="input" name="userComments" value="Observaciones adicionales" />
+          </div>
+        </div>
+        <div className="booking-body__pay">
+          <h5>Detalles del pago</h5>
+        </div>
+        <div className="booking-body__summary">
+          <BookingSummary />
+        </div>
+      </div>
       <Footer />
     </div>
   );
