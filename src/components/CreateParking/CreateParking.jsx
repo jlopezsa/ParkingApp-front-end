72fc8ceefd6ced400e115d941904f1470d8f6334
lmportoesq <lmportoesq@gmail.com>
2022-03-31 11:15:09 -0500

    add direction,phone,length and latitud

--- src/components/CreateParking/CreateParking.jsx
+++ src/components/CreateParking/CreateParking.jsx
@@ -7,10 +7,14 @@ function CreateParking() {
       <form className="container__form">
         <input className="container__form--input" type="text" name="name" placeholder="Nombre del parqueadero" />
         <input className="container__form--input" type="text" name="city" placeholder="Ciudad" />
-        <input className="container__form--input" type="number" name="price" placeholder="Tarifa" />
+        <input className="container__form--input" type="text" name="adress" placeholder="Dirección" />
+        <input className="container__form--input" type="number" name="phone" placeholder="Teléfono" />
+        <input className="container__form--input" type="number" name="price" placeholder="Valor hora" />
         <input className="container__form--input" type="time" name="startTime" placeholder="Hora de apertura" />
         <input className="container__form--input" type="time" name="endTime" placeholder="Hora de cierre" />
-        <input className="container__form--input" type="number" name="quotas" placeholder="Total cupos disponibles" />
+        <input className="container__form--input" type="number" name="sites" placeholder="Número de puestos" />
+        <input className="container__form--input" type="number" name="length" placeholder="Longitud" />
+        <input className="container__form--input" type="number" name="latitude" placeholder="Latitud" />
         <input className="container__form--input" type="imagen" name="image" placeholder="Imagen" />
         <button type="submit" className="container__form--Button">Registrar</button>
         <button type="submit" className="container__form--Button">Cancelar</button>
