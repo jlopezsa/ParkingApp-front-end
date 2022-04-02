579dc1e9e753a6a54f9b2ada8f7abafac4261fcf
Julian Lopez <jlopezsa@gmail.com>
2022-04-02 07:27:39 -0500

    feat2: Adjust colors of hamburguer menu

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -4,7 +4,7 @@ import './NavBar.scss';
 
 function NavBar() {
   return (
-    <div className="container-navbar">
+    <div>
       <nav className="container-menu">
         <ul className="container-menu__list">
           <li><NavLink className="menu__list--inactive" to="/">Mi cuenta</NavLink></li>
@@ -18,7 +18,9 @@ function NavBar() {
       <input className="burger-shower" type="checkbox" id="hamburger-input" />
       <label id="hamburger-menu" htmlFor="hamburger-input">
         <nav id="sidebar-menu">
-          <h3>Menu</h3>
+          <div id="sidebar-menu__title">
+            <h3>Menu</h3>
+          </div>
           <ul className="hamburguer-menu__list">
             <li><NavLink className="hamburguer__list--inactive" to="/">Mi cuenta</NavLink></li>
             <li><NavLink className="hamburguer__list--inactive" to="/LogIn">Ingresar</NavLink></li>
