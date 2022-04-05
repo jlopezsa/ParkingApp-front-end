cf29a133d43d89e7a81665146334fdefbdc7fe4b
Julian Lopez <jlopezsa@gmail.com>
2022-04-05 11:07:29 -0500

    feat: Add proptypes in Maps component to resolve conflict with eslint

--- src/components/Maps/Maps.jsx
+++ src/components/Maps/Maps.jsx
@@ -1,55 +1,48 @@
-/* eslint-disable*/
 import React, { useState, useEffect } from 'react';
 import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
+import PropTypes from 'prop-types';
 
 import { getAllParkings } from '../../services/parkings';
-
 
 const containerStyle = {
   width: '100%',
   height: '100%',
 };
 
-const onLoad = (marker) => {
-};
-
-function Maps(props) {
-  const { searchCity } = props;
-
+function Maps({ searchCity }) {
   const [parkings, setParkings] = useState([]);
-  const [centerCoor, setCenterCords] = useState({"lat":4.65, "long":-74.1});
+  const [centerCoor, setCenterCords] = useState({ lat: 4.65, long: -74.1 });
 
   useEffect(() => {
     const fetchParkings = async () => {
       const data = await getAllParkings();
-      const filterData = data.filter((item)=> item.city.cityName === searchCity);
+      const filterData = data.filter((item) => item.city.cityName === searchCity);
       setParkings(filterData);
-      if(!searchCity){
-        setCenterCords({"lat":4.65, "long":-74.1})
-      }else{
-        setCenterCords({"lat":filterData[0].city.latitude, "long":filterData[0].city.longitude})
+      if (!searchCity) {
+        setCenterCords({ lat: 4.65, long: -74.1 });
+      } else {
+        setCenterCords({ lat: filterData[0].city.latitude, long: filterData[0].city.longitude });
       }
     };
     fetchParkings();
   }, [searchCity]);
 
-
   return (
     <LoadScript
-      googleMapsApiKey = {process.env.REACT_APP_KEY_MAPS}
+      googleMapsApiKey={process.env.REACT_APP_KEY_MAPS}
     >
       <GoogleMap
         mapContainerStyle={containerStyle}
         center={{
-          "lat": centerCoor.lat,
-          "lng": centerCoor.long
+          lat: centerCoor.lat,
+          lng: centerCoor.long,
         }}
         zoom={11}
       >
         { /* Child components, such as markers, info windows, etc. */}
         {
           parkings.map((item) => (
-            <Marker key={item.id} onLoad={onLoad} position={item.coords} />
+            <Marker key={item.id} position={item.coords} />
           ))
         }
       </GoogleMap>
@@ -57,5 +50,11 @@ function Maps(props) {
 
   );
 }
+
+Maps.propTypes = {
+  searchCity: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+  }).isRequired,
+};
 
 export default React.memo(Maps);
