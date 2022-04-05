a2bdc7872064928df504cda79265d2a332c6f550
Julian Lopez <jlopezsa@gmail.com>
2022-04-04 21:01:26 -0500

    feat: create service getOneParking

--- src/services/parkings.js
+++ src/services/parkings.js
@@ -4,8 +4,17 @@ const API_URL = 'http://localhost:8080/api';
 export async function getAllParkings() {
   try {
     const response = await fetch(`${API_URL}/parking`);
-    const data = await response.json();
-    return data;
+    const parkings = await response.json();
+    return parkings;
+  } catch (error) {
+    console.log(error);
+  }
+}
+export async function getOneParking(id) {
+  try {
+    const response = await fetch(`${API_URL}/parking/${id}`);
+    const parking = await response.json();
+    return parking;
   } catch (error) {
     console.log(error);
   }
