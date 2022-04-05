83b7b5c6e43de73e5a20a0c089adcfe9fa1d5cc4
Julian Lopez <jlopezsa@gmail.com>
2022-04-05 12:37:29 -0500

    feat: Delete console.log() as comments

--- src/services/parkings.js
+++ src/services/parkings.js
@@ -6,7 +6,6 @@ export async function getAllParkings() {
     const parkings = await response.json();
     return parkings;
   } catch (error) {
-    // console.log(error);
     return null;
   }
 }
@@ -16,7 +15,6 @@ export async function getOneParking(id) {
     const parking = await response.json();
     return parking;
   } catch (error) {
-    // console.log(error);
     return null;
   }
 }
