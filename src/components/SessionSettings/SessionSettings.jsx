5e353111fe10439957a9329751cc059725bbc9b4
Julian Lopez <jlopezsa@gmail.com>
2022-04-05 23:21:02 -0500

    feat: Create files of the SessionSettins component

---
+++ src/components/SessionSettings/SessionSettings.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function SessionSettings() {
+  return (
+    <div>
+      <h1>Session settings component</h1>
+    </div>
+  );
+}
+
+export default SessionSettings;
--- src/pages/AdminPage.jsx
+++ src/pages/AdminPage.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
+import Footer from '../components/Footer/Footer';
+import Header from '../components/Header/Header';
+import SessionSettings from '../components/SessionSettings/SessionSettings';
 import './AdminPage.scss';
 
 function AdminPage() {
   return (
-    <div>
+    <div className="container-admin">
+      <Header />
       <h1>
         Admin Page
       </h1>
+      <SessionSettings />
+      <Footer />
     </div>
   );
 }
