fa8eecd1eaa07967f82c564e1b8d77467e3c78b1
Julian Lopez <jlopezsa@gmail.com>
2022-04-06 00:12:11 -0500

    feat: Add user information

--- src/components/SessionSettings/SessionSettings.jsx
+++ src/components/SessionSettings/SessionSettings.jsx
@@ -1,9 +1,43 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+import './SessionSettings.scss';
 
 function SessionSettings() {
   return (
-    <div>
-      <h1>Session settings component</h1>
+    <div className="sessionsettings">
+      <div className="sessionsettings__header">
+        <div className="sessionsettings__header--figure">
+          <p>figuras</p>
+        </div>
+        <div className="sessionsettings__header--info">
+          <p>Mi cuenta (Administrador)</p>
+          <Link to="https://github.com/makeitrealcamp/top-v20">Cambiar fotografia</Link>
+        </div>
+        <div className="sessionsettings__header--button">
+          <button type="submit">Salir</button>
+        </div>
+      </div>
+      <div className="sessionsettings__title">
+        <p className="sessionsettings__title-p">Ajustes de sesión</p>
+      </div>
+      <div className="sessionsettings__data">
+        <p className="sessionsettings__data-p">Nombre:</p>
+        <div className="sessionsettings__data--name">
+          <p>Jano Cooper</p>
+          <Link to="https://github.com/makeitrealcamp/top-v20">Cambiar</Link>
+        </div>
+        <p className="sessionsettings__data-p">Email:</p>
+        <div className="sessionsettings__data--email">
+          <p>ckasd@mail.com</p>
+          <Link to="https://github.com/makeitrealcamp/top-v20">Cambiar</Link>
+        </div>
+        <p className="sessionsettings__data-p">Password:</p>
+        <div className="sessionsettings__data--password">
+          <p>********</p>
+          <Link to="https://github.com/makeitrealcamp/top-v20">Cambiar</Link>
+        </div>
+      </div>
     </div>
   );
 }
