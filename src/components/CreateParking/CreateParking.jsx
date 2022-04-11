ce15a5a8c3b50d5747f25c406faeacb380e5d1a1
lmportoesq <82108153+lmportoesq@users.noreply.github.com>
2022-04-11 15:48:00 -0500

    Feature/create parkig (#32)
    * create folder CreateParking
    
    * create component CreateParking
    
    * create component CreateParking
    
    * create CreateParking.scss file
    
    * create CreateParking.scss styles
    
    * add direction,phone,length and latitud
    
    * rename classNames
    
    * create css styles
    
    * add create parking link
    
    * create CreateParkings page
    
    * create CreateParkings page route
    
    * rename CreateParkings.jsx to CreateParkingsPage.jsx
    
    * call CreateParkingPage
    
    * defining css styles
    
    * modify styles css
    
    * fix conflicts
    
    Co-authored-by: lmportoesq <lmportoesq@gmail.com>

---
+++ src/components/CreateParking/CreateParking.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import './CreateParking.scss';
+
+function CreateParking() {
+  return (
+    <div className="container">
+      <h2>Ingrese datos del parqueadero</h2>
+      <form id="form">
+        <input className="container__input" type="text" name="name" placeholder="Nombre del parqueadero" />
+        <input className="container__input" type="text" name="city" placeholder="Ciudad" />
+        <input className="container__input" type="text" name="adress" placeholder="Dirección" />
+        <input className="container__input" type="number" name="phone" placeholder="Teléfono" />
+        <input className="container__input" type="number" name="price" placeholder="Valor hora" />
+        <input className="container__input" type="time" name="startTime" placeholder="Hora de apertura" />
+        <input className="container__input" type="time" name="endTime" placeholder="Hora de cierre" />
+        <input className="container__input" type="number" name="sites" placeholder="Número de puestos" />
+        <input className="container__input" type="number" name="length" placeholder="Longitud" />
+        <input className="container__input" type="number" name="latitude" placeholder="Latitud" />
+        <input className="container__input" type="text" name="image" placeholder="Imagen" />
+        <button className="container__Button" type="submit">REGISTRAR</button>
+        <button className="container__Button" type="submit">CANCELAR</button>
+      </form>
+    </div>
+  );
+}
+export default CreateParking;
