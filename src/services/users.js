b2f27abd1e3405cd6a2728f3e982eb8bcd5a4c14
Julian Lopez <jlopezsa@gmail.com>
2022-04-12 17:45:40 -0500

    feat: Connection front-end with back-end to reister an user

---
+++ src/services/users.js
@@ -0,0 +1,23 @@
+const API_URL = process.env.REACT_APP_BASE_URL;
+
+async function createUser(user) {
+  const payload = {
+    method: 'POST',
+    headers: {
+      'Content-type': 'application/json',
+    },
+    body: JSON.stringify(user),
+  };
+
+  try {
+    const response = await fetch(`${API_URL}/api/users`, payload);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    throw new Error(error);
+  }
+}
+
+module.exports = {
+  createUser,
+};
