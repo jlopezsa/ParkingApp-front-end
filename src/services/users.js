8e95b87e390b7e6fe8959555309fe01c2b36c0c0
Julian Lopez <jlopezsa@gmail.com>
2022-04-13 09:28:47 -0500

    feat: CREATE, DELETE and UPDATE methods for parking and user services

--- src/services/users.js
+++ src/services/users.js
@@ -17,3 +17,21 @@ export async function createUser(user) {
     throw new Error(error);
   }
 }
+
+export async function updateUser(user) {
+  const payload = {
+    method: 'PATCH',
+    headers: {
+      'Content-type': 'application/json',
+    },
+    body: JSON.stringify(user),
+  };
+
+  try {
+    const response = await fetch(`${API_URL}/api/users/${user.id}`, payload);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    throw new Error(error);
+  }
+}
