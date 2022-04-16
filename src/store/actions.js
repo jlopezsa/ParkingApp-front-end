650f282fb3c4d23712e63b2983c63ca58a7e90bd
Julian Lopez <jlopezsa@gmail.com>
2022-04-16 00:09:52 -0500

    feat: Create files and structure to use redux-thunk

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,8 +1,42 @@
-/* eslint-disable arrow-parens */
-// eslint-disable-next-line import/prefer-default-export
-export const searchCity = val => ({
-  type: 'SEARCH_CITY',
+import {
+  SET_PARKINGS,
+  SEARCH_CITY,
+} from './types';
+
+import {
+  filteringParkingByCity, getAllParkings,
+} from '../services/parkings';
+
+export const setParkings = (parkings) => ({ type: SET_PARKINGS, payload: parkings });
+
+export const listParkingsByCity = (city) => async (dispatch) => {
+  try {
+    const parkings = await filteringParkingByCity(city);
+    dispatch(setParkings(parkings));
+  } catch (error) {
+    console.log('FLAG-A: listParkingsByCity');
+  }
+};
+
+export const searchCity = (val) => ({
+  type: SEARCH_CITY,
   payload: val,
   error: false,
   meta: {},
 });
+
+export const getParkings = () => async (dispatch) => {
+  try {
+    const parkings = await getAllParkings();
+    dispatch(setParkings(parkings));
+  } catch (error) {
+    console.log('FLAG-A: getParkings');
+  }
+};
+
+// export const getParkings = (parkings) => ({
+//   type: GET_ALL_PARKINGS,
+//   payload: val,
+//   error: false,
+//   meta: {},
+// });
--- src/store/reducers.js
+++ src/store/reducers.js
@@ -1,15 +1,32 @@
+import {
+  SET_PARKINGS,
+  SEARCH_CITY,
+} from './types';
+
 const initialState = {
   searchCity: '',
+  parkingsFiltered: [],
 };
 
 // eslint-disable-next-line default-param-last
 function reducer(state = initialState, action) {
   switch (action.type) {
-    case 'SEARCH_CITY':
+    case SEARCH_CITY:
       return {
         ...state,
         searchCity: action.payload,
       };
+    case SET_PARKINGS:
+      return {
+        ...state,
+        parkingsFiltered: action.payload,
+      };
+      // case 'GET_ALL_PARKINGS':
+      //   return {
+      //     ...state,
+      //     parkingsFiltered: action.payload,
+      //   };
+
     default:
       return state;
   }
