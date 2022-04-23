5c3f10b49896caa5c45fd2de601db22a2629c9bc
Julian Lopez <jlopezsa@gmail.com>
2022-04-22 19:23:56 -0500

    feat: Add user token by hard code

--- src/services/payments.js
+++ src/services/payments.js
@@ -1,10 +1,12 @@
 const API_URL = process.env.REACT_APP_LOCAL_URL;
 
 export async function createTokenCard(paymentMethod) {
+  console.log(paymentMethod);
   const payload = {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJmdWxsTmFtZSI6IkpVTElBTiBsb3BleiIsInJvbGUiOiJhZG1pbiIsImVtYWlsIjoianVsaWFuQHBhcmtpbmcuY29tIiwiaWF0IjoxNjUwNjY1NTc0LCJleHAiOjE2NTA2NzI3NzR9.PD1D6RZbX1HhhEKOF7bq0so98MReej-lrM1rGXU7VIg',
     },
     body: JSON.stringify({
       paymentMethod,
