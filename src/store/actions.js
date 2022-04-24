cb6c071d6aca30cdb7f4a919fac434a7581f8663
Julian Jair Lopez Salamanca <jlopezsa@gmail.com>
2022-04-24 09:51:00 -0500

    Feature/redux thunk (#37)
    * feat: Create files and structure to use redux-thunk
    
    * feat: Install Redux-Thunk
    
    * feat: List parkings filtered by city and using Redux-Thunk
    
    * feat: Update Id admin to list parkings
    
    * feat: Delete comments

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,8 +1,29 @@
-/* eslint-disable arrow-parens */
-// eslint-disable-next-line import/prefer-default-export
-export const searchCity = val => ({
-  type: 'SEARCH_CITY',
-  payload: val,
-  error: false,
-  meta: {},
-});
+import {
+  SET_PARKINGS,
+  SEARCH_CITY,
+} from './types';
+
+import {
+  filteringParkingByCity, getAllParkings,
+} from '../services/parkings';
+
+export const setParkings = (parkings) => ({ type: SET_PARKINGS, payload: parkings });
+export const searchCity = (city) => ({ type: SEARCH_CITY, payload: city, error: false });
+
+export const listParkingsByCity = (city) => async (dispatch) => {
+  try {
+    const parkings = await filteringParkingByCity(city);
+    dispatch(setParkings(parkings));
+  // eslint-disable-next-line no-empty
+  } catch (error) {
+  }
+};
+
+export const getParkings = () => async (dispatch) => {
+  try {
+    const parkings = await getAllParkings();
+    dispatch(setParkings(parkings));
+  // eslint-disable-next-line no-empty
+  } catch (error) {
+  }
+};
--- src/store/reducers.js
+++ src/store/reducers.js
@@ -1,15 +1,27 @@
+import {
+  SET_PARKINGS,
+  SEARCH_CITY,
+} from './types';
+
 const initialState = {
   searchCity: '',
+  parkingsFiltered: [],
 };
 
 // eslint-disable-next-line default-param-last
 function reducer(state = initialState, action) {
   switch (action.type) {
-    case 'SEARCH_CITY':
+    case SEARCH_CITY:
       return {
         ...state,
         searchCity: action.payload,
       };
+    case SET_PARKINGS:
+      return {
+        ...state,
+        parkingsFiltered: action.payload,
+      };
+
     default:
       return state;
   }
