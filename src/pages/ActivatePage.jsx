2d24e6d6fa90d00b52904cca885ee37b717cee3d
Luis porto <lmportoesq@gmail.com>
2022-04-24 20:59:42 -0500

    fix: organized the code instruccions

--- src/pages/ActivatePage.jsx
+++ src/pages/ActivatePage.jsx
@@ -1,17 +1,26 @@
 import { useEffect } from 'react';
 import { useParams, Navigate } from 'react-router-dom';
 
-const { token } = useParams();
-const navigate = Navigate();
+const URL_BASE = 'http://localhost:8080';
 
-const activate = async () => {
-  const response = await fetch(`${URL_BASE}/auth/local/verify-account/${token}`);
-  const data = await response.json();
-  if (data.token) {
-    localStorage.setItem('token', data.token);
-    navigate('/');
-  }
+function Activate() {
+  const { token } = useParams();
+  const navigate = Navigate();
+  const activate = async () => {
+    const response = await fetch(`${URL_BASE}/auth/local/verify-account/${token}`);
+    const data = await response.json();
+
+    if (data.token) {
+      localStorage.setItem('token', data.token);
+      navigate('/');
+    }
+  };
   useEffect(() => {
     activate();
   }, []);
-};
+
+  return (
+    <div>Bienvenid@</div>
+  );
+}
+export default Activate;
