aee01fb2903515dfdc758de1d724f71576fe8014
AndySast <andressastoquexyz@gmail.com>
2022-04-28 08:00:38 -0500

    creationPageWelcome

--- src/pages/Welcome.jsx
+++ src/pages/Welcome.jsx
@@ -4,9 +4,28 @@ import './Welcome.styles.scss';
 
 function Welcome() {
   return (
-    <div className="container-welcome">
-      <img src={parkingTitle} alt="title" />
-      <p> Welcome to the Parking App!</p>
+    <div className="body__welcome">
+      <div className="welcome">
+        <img className="welcome__img" src={parkingTitle} alt="title" />
+      </div>
+      <div className="target">
+        <div className="target__text">
+          <h1 className="target__text__title">
+            Bienvenido !
+          </h1>
+          <p className="target__text__description">
+            Su cuenta de usuario se ha creado con éxito.
+          </p>
+        </div>
+        <div className="target__buttons">
+          <button className="target__buttons__a" type="button">
+            Continuar con mi reserva
+          </button>
+          <button className="target__buttons__b" type="button">
+            Aceptar
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
