342ac5e6995163511ac92d69efc40cf2bca2a2a2
Luis porto <lmportoesq@gmail.com>
2022-04-27 21:27:55 -0500

    fix: eslint roules

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,19 +1,19 @@
-/* eslint-disable */
 import './Login.scss';
+
 function Login() {
   return (
-    <>
-        <form className="form">
-          <h2 className='form__h2'>Ingresar</h2>
-          <input className="form__input" type="email" name="email" placeholder="email" />
-          <input className="form__input" type="password" name="password" placeholder="Contraseña" />
+    <div>
+      <form className="form">
+        <h2 className="form__h2">Ingresar</h2>
+        <input className="form__input" type="email" name="email" placeholder="email" />
+        <input className="form__input" type="password" name="password" placeholder="Contraseña" />
 
-          <p>Olvidó la contraseña?</p>
-          <input className="form__submit" type="submit" name="submit" value="Ingresar" />
-          <p>No tiene cuenta? Registrese ahora!</p>
+        <p>Olvidó la contraseña?</p>
+        <input className="form__submit" type="submit" name="submit" value="Ingresar" />
+        <p>No tiene cuenta? Registrese ahora!</p>
 
-        </form>
-    </>
-  )
+      </form>
+    </div>
+  );
 }
 export default Login;
