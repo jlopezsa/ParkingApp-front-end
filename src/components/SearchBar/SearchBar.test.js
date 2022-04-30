f08f25ebc70e990b1a651e24914c2469551c86cc
Julian Lopez <jlopezsa@gmail.com>
2022-04-30 07:07:45 -0500

    feat: Deleted unnecesary librar

--- src/components/SearchBar/SearchBar.test.js
+++ src/components/SearchBar/SearchBar.test.js
@@ -1,5 +1,5 @@
 /* eslint-disable */
-import { render, screen, fireEvent } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import configureStore from "redux-mock-store";
 import SearchBar from './SearchBar';
