9b43111c1da824a32505e25f93f43bb421d05936
Julian Lopez <jlopezsa@gmail.com>
2022-05-01 00:54:52 -0500

    feat: Upload image from frontend

--- src/components/CreateParking/CreateParking.jsx
+++ src/components/CreateParking/CreateParking.jsx
@@ -1,12 +1,44 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
-import React from 'react';
+import { React, useState } from 'react';
 import './CreateParking.scss';
 
 function CreateParking() {
+  const [parkingData, setParkingData] = useState({});
+  const [parkingImage, setParkingImage] = useState(null);
+
+  const handleChange = (e) => {
+    if (!e.target.files) {
+      setParkingData({
+        ...parkingData,
+        [e.target.name]: e.target.value,
+      });
+    } else {
+      setParkingImage(e.target.files[0]);
+    }
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const formData = new FormData();
+    formData.append('file', parkingImage);
+
+    const payload = {
+      method: 'POST',
+      body: formData,
+    };
+
+    try {
+      const result = await fetch('http://localhost:3035/api/upload/image', payload);
+      console.log('RESULTADO: ', result);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div className="container-create">
       <h2>Ingrese datos del parqueadero</h2>
-      <form id="form">
+      <form id="form" onChange={handleChange} onSubmit={handleSubmit}>
         <input className="container-create__input" type="text" name="name" placeholder="Nombre del parqueadero" />
         <input className="container-create__input" type="text" name="city" placeholder="Ciudad" />
         <input className="container-create__input" type="text" name="adress" placeholder="Dirección" />
@@ -19,7 +51,8 @@ function CreateParking() {
         <input className="container-create__input" type="number" name="sites" placeholder="Número de puestos" />
         <input className="container-create__input" type="number" name="length" placeholder="Longitud" />
         <input className="container-create__input" type="number" name="latitude" placeholder="Latitud" />
-        <input className="container-create__input" type="text" name="image" placeholder="Imagen" />
+        <label htmlFor="image">Subir imagen de parqueadero</label>
+        <input className="container-create__input" type="file" name="image" placeholder="Imagen" accept="image/*" />
         <button className="container-create__Button" type="submit">REGISTRAR</button>
         <button className="container-create__Button" type="submit">CANCELAR</button>
       </form>
--- src/pages/CreateParkingsPage.jsx
+++ src/pages/CreateParkingsPage.jsx
@@ -1,18 +1,17 @@
-/* eslint-disable */
 import React from 'react';
 
 import CreateParking from '../components/CreateParking/CreateParking';
 import Footer from '../components/Footer/Footer';
 import Header from '../components/Header/Header';
 
-function CreateParkingsPage(){
-  return(
-    <div >
+function CreateParkingsPage() {
+  return (
+    <div>
       <Header />
       <CreateParking />
       <Footer />
     </div>
-  )
-};
+  );
+}
 
 export default CreateParkingsPage;
