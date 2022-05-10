590b72b9d58218d8ae6a646e5fc05fe30b27b10f
Julian Jair Lopez Salamanca <jlopezsa@gmail.com>
2022-05-10 16:54:30 -0500

    feat: Update info of the home page (#69)

--- src/components/Views/Views.jsx
+++ src/components/Views/Views.jsx
@@ -25,21 +25,23 @@ const Views = () => {
 
       <ViewsFigRight title={'¿Qué hacemos?'} figure={rigthFig} text={'Permitir realizar la conección entre usuarios que requieren estacionar sus vehículos y parqueaderos que tengan disponibilidad.'}/>
 
-      <ViewsFigTopLeft title={'¿Cómo lo hacemos?'} figure={parkingWoman} text={'Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime assumenda soluta, quaerat mollitia sequi explicabo eaque dolor voluptatum animi dolorum magni beatae laboriosam? Quia, excepturi. Veniam doloremque aperiam deserunt nisi.'}/>
-      <ViewsHorizontal titleLeft={''} figureLeft={parkingManSmartphone} textLeft={'dolor sit amet consectetur adipisicing elit. Maxime assumenda soluta, quaerat mollitia sequi explicabo eaque dolor voluptatum animi dolorum magni beatae laboriosam? Quia, excepturi.'} titleRight={''} figureRight={parkingWomanBike} textRight={'Quia, excepturi. Veniam doloremque aperiam deserunt nisi. g elit. Velit cupiditate nesciunt hic nam. Magni, illo.'} />
+      <ViewsFigTopLeft title={'¿Cómo lo hacemos?'} figure={parkingWoman} text={'Haciendo uso de las diferentes herramientas tecnológicas y poniendolas a disposición del usuario, para que pueda buscar y seleccionar el parqueadero que cumpa con sus necesidades o requerimientos.'}/>
 
-      <div className="container-title">
-        <p>¿Y los parquaderos?</p>
-      </div>
-
-      <ViewsHorizontal titleLeft={''} figureLeft={datafonoMan} textLeft={'Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime assumenda soluta, quaerat mollitia sequi explicabo eaque dolor voluptatum animi dolorum magni beatae laboriosam? Quia, excepturi. Veniam doloremque aperiam deserunt nisi.'} titleRight={''} figureRight={statisticsDoc} textRight={'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit cupiditate nesciunt hic nam. Magni, illo. Dignissimos at adipisci consequatur? Esse vitae cumque eius vero nihil atque eveniet, nostrum autem voluptatum? Adipisci iste, vel rerum quidem vitae natus voluptas laboriosam atque repudiandae.'} />
-
-      <ViewsFigTopRight title={''} figure={presentationMan} text={'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit cupiditate nesciunt hic nam. Magni, illo. Dignissimos at adipisci consequatur? consectetur adipisicing elit. Velit cupiditate nesciunt hic nam. Magni, illo. Dignissimos at adipisci consequatur? Esse vitae cumque eius vero nihil atque eveniet, nostrum autem voluptatum? Adipisci iste, vel rerum quidem vitae'}/>
+      <ViewsHorizontal titleLeft={''} figureLeft={datafonoMan} textLeft={'La posibilidad de incorporar pagos con tarjeta, así mismo permitir el cargue de estas transacciones a la pagina'} titleRight={''} figureRight={statisticsDoc} textRight={'El administrador tendrá informes y la opción de explorar sus transacciones en lapsos como días o semanas, '} />
 
       <div className="container-title">
         <p>Proximamente...</p>
       </div>
-      <ViewsFigTopLeft title={''} figure={future} text={'Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime assumenda soluta, quaerat mollitia sequi explicabo eaque dolor voluptatum animi dolorum magni beatae laboriosam? Quia, excepturi. Veniam doloremque aperiam deserunt nisi.'}/>
+
+      <ViewsHorizontal titleLeft={''} figureLeft={parkingManSmartphone} textLeft={'Implementar un chat entre el usuario y el administrador con el fin de que exista una comunicación dirécta y segura a través de la aplicación.'} titleRight={''} figureRight={parkingWomanBike} textRight={'Una véz realizada la reserva la plataforma podrá guiar al usuario desde el origen hasta la dirección del parqueadero.'} />
+
+
+      <ViewsFigTopRight title={''} figure={presentationMan} text={'Estadísticas, el administrador tendrá una data robusta como horas valle, horas pico, y listado o informes de usuarios que utilizan la plataforma. Con estos datos se podrán realizar predicciones de los días y horas de mayor concorrencia.'}/>
+
+      <div className="container-title">
+        <p></p>
+      </div>
+      <ViewsFigTopLeft title={''} figure={future} text={'Aumentar la cobertura, pensamos expandirnos a ciudades o poblaciones mas pequeñas teniendo una mayor cuota de mercado.'}/>
 
 
     </div>
